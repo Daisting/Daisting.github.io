242560401464e1f181a3b87893521cf21f6b45b7
Daisting <100518866+Daisting@users.noreply.github.com>
2022-04-09 00:02:31 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function writeAnswer(){
+  var response = prompt("who is the killer?");
+  if (response == "王奇"){
+  alert("Congratulations!You deserve to be a famous dective!");
+}
+  else {
+  alert("The famous dective could also make mistakes,please try again.")
+  }
+}
+
+function checkAnswer(){
+  alert("The poison is on the pushpin tip of the seat. When pulling the stool, the pushpin pierces your finger and is poisoned. The killer is B. The clerk chewed gum. The pushpin was stuck under the stool with gum")
+}
